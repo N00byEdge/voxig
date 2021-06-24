263704471f449f16b036b035afc437931f651155
N00byEdge <hannesbredberg@gmail.com>
2021-06-24 22:38:00 +0200

    Fix fragment shader warning

--- src/shaders/voxel.fs
+++ src/shaders/voxel.fs
@@ -2,9 +2,10 @@
 
 in vec3 shaded_text_coord;
 
+layout(location = 0) out vec4 color;
+
 uniform sampler2DArray texture_sampler;
 
 void main() {
-  gl_FragColor = texture(texture_sampler, shaded_text_coord);
-  //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  color = texture(texture_sampler, shaded_text_coord);
 }
