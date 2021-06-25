760fe226af6d730fec0d9e892ca292bf80874a47
N00byEdge <hannesbredberg@gmail.com>
2021-06-25 12:09:19 +0200

    Remove unused config setting

--- config.zig
+++ config.zig
@@ -2,10 +2,6 @@ pub const textures = .{
     .dim = 16,
 };
 
-// If vertex lists should be generated conditionally
-// Slows down chunk generation, but generates a smaller chunk mesh
-pub const conditional_vertices = true;
-
 pub const fov = 90;
 pub const near = 0.01;
 pub const far = 10000;
