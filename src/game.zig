eac023536b7968d3ce8b0351b0d8b10b7b0b3840
N00byEdge <hannesbredberg@gmail.com>
2021-06-26 23:02:35 +0200

    Game: Consistent function naming

--- src/game.zig
+++ src/game.zig
@@ -20,13 +20,13 @@ fn forward(look_x: f32, look_z: f32) glm.Vector(3) {
     });
 }
 
-fn config_key_pressed(window: anytype, comptime tag: anytype) bool {
+fn configKeyPressed(window: anytype, comptime tag: anytype) bool {
     const key_name = @tagName(@field(config.keys, @tagName(tag)));
     const state = glfw.c.glfwGetKey(window, @field(glfw.c, key_name));
     return state == glfw.c.GLFW_PRESS;
 }
 
-fn update_mouse(window: anytype, look_x: *f32, look_z: *f32) void {
+fn updateMouse(window: anytype, look_x: *f32, look_z: *f32) void {
     const delta = glfw.getMouseDelta(window);
 
     look_x.* += @floatCast(f32, -delta.dx * config.mouse_sensitivity);
@@ -37,21 +37,21 @@ fn update_mouse(window: anytype, look_x: *f32, look_z: *f32) void {
     look_z.* = std.math.clamp(look_z.*, -@as(f32, std.math.pi) / 2, @as(f32, std.math.pi) / 2);
 }
 
-fn update_movement(window: anytype, pos: *glm.Vector(3), look_x: f32) void {
+fn updateMovement(window: anytype, pos: *glm.Vector(3), look_x: f32) void {
     const forwards = forward(look_x, 0);
     const right = forward(look_x - @as(f32, std.math.pi) / 2, 0);
 
     var vel = glm.Vector(3).init([_]f32{ 0, 0, 0 });
 
-    if (config_key_pressed(window, .forward)) vel.addAssign(forwards);
-    if (config_key_pressed(window, .backward)) vel.subAssign(forwards);
-    if (config_key_pressed(window, .left)) vel.subAssign(right);
-    if (config_key_pressed(window, .right)) vel.addAssign(right);
+    if (configKeyPressed(window, .forward)) vel.addAssign(forwards);
+    if (configKeyPressed(window, .backward)) vel.subAssign(forwards);
+    if (configKeyPressed(window, .left)) vel.subAssign(right);
+    if (configKeyPressed(window, .right)) vel.addAssign(right);
 
     if (vel.values[0] != 0 or vel.values[1] != 0) vel.normalizeAssign();
 
-    if (config_key_pressed(window, .down)) vel.values[2] -= 1;
-    if (config_key_pressed(window, .up)) vel.values[2] += 1;
+    if (configKeyPressed(window, .down)) vel.values[2] -= 1;
+    if (configKeyPressed(window, .up)) vel.values[2] += 1;
 
     // We just assume 60 fps here, should use the frame delta at some point
     vel.mulAssignScalar(config.movement_speed / @as(f32, 60));
@@ -108,12 +108,12 @@ pub fn loop(game_window: anytype) !void {
     var look_x: f32 = @as(f32, std.math.pi) / 2;
 
     while (true) {
-        if (glfw.windowShouldClose(game_window) or config_key_pressed(game_window, .quit)) {
+        if (glfw.windowShouldClose(game_window) or configKeyPressed(game_window, .quit)) {
             break;
         }
 
-        update_mouse(game_window, &look_x, &look_z);
-        update_movement(game_window, &position, look_x);
+        updateMouse(game_window, &look_x, &look_z);
+        updateMovement(game_window, &position, look_x);
 
         const look_direction = forward(look_x, look_z);
         const up = forward(look_x, look_z + @as(f32, std.math.pi) / 2);
